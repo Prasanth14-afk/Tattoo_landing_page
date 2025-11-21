266cd81d30920bc268a5c757cd5dfca89bc96cfc
prasanthkumar <prasanthkumar@example.com>
2025-11-21 16:37:27 +0530

    Fix: Replace @studio-freight/react-lenis with lenis to resolve peer dependency conflicts

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
-    return (
-        <ReactLenis root options={{ lerp: 0.07, duration: 1.2, smoothWheel: true }}>
-            {children as any}
-        </ReactLenis>
-    );
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return <>{children}</>;
 }
